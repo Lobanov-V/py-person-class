a451594334f64be8f8571470e1858d282ce13c3c
Volodymyr Lobanov <lobanovolodymyr@icloud.com>
2025-12-14 14:15:02 +0700

    Made changes for Luke

--- app/main.py
+++ app/main.py
@@ -8,22 +8,23 @@ class Person:
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
-    person_list: list[Person] = []
+    Person.people = {}
 
-    for person_data in people:
-        person = Person(
-            name=person_data["name"],
-            age=person_data["age"],
-        )
-        person_list.append(person)
+    person_list = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
 
     for person_data in people:
         person = Person.people[person_data["name"]]
 
-        if "wife" in person_data and person_data["wife"] is not None:
-            person.wife = Person.people[person_data["wife"]]
+        wife_name = person_data.get("wife")
+        if wife_name is not None:
+            person.wife = Person.people[wife_name]
 
-        if "husband" in person_data and person_data["husband"] is not None:
-            person.husband = Person.people[person_data["husband"]]
+        husband_name = person_data.get("husband")
+        if husband_name is not None:
+            person.husband = Person.people[husband_name]
 
     return person_list
+
